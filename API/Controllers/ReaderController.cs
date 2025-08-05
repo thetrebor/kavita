ec60addaa035730f50955ad6de5e52be05d4f49d
Maximo Piva <maximo.piva@gmail.com>
2025-08-05 14:08:23 -0300

    Update ReaderController.cs

--- API/Controllers/ReaderController.cs
+++ API/Controllers/ReaderController.cs
@@ -132,7 +132,6 @@ public class ReaderController : BaseApiController
         }
         catch (Exception e)
         {
-            _logger.LogError(e.Message, e);
             _cacheService.CleanupChapters([chapterId]);
             throw;
         }
