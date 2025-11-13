d75fcaff84131ad88f486b64a0e9e635c8d9c6ca
Joe Milazzo <josephmajora@gmail.com>
2025-11-13 14:43:34 -0600

    Added a missing localization string

--- UI/Web/src/app/statistics/_components/fiction-graph/fiction-graph.component.ts
+++ UI/Web/src/app/statistics/_components/fiction-graph/fiction-graph.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {TranslocoDirective} from "@jsverse/transloco";
 
 @Component({
   selector: 'app-fiction-graph',
-  imports: [],
+  imports: [
+    TranslocoDirective
+  ],
   templateUrl: './fiction-graph.component.html',
   styleUrl: './fiction-graph.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
